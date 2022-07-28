14dcc0bc850a4712be4f2536ce1a38e297b61a86
Tobin <tobinsmusic@gmail.com>
2022-07-28 15:13:13 +0000

    added station model

---
+++ models/stations.sql
@@ -0,0 +1,16 @@
+with stations as (
+
+    select
+        call_sign,
+        short_common_name
+
+    from public.core_station
+),
+final as (
+select
+stations.call_sign,
+stations.short_common_name
+from stations
+
+)
+select * from final
